e0b15bc777f1c5d1a626514a3dbbb8543ca8de48
Cynthia Nyeint <cynthianyeint@Cynthias-MacBook-Pro.local>
2017-03-16 16:58:08 +0800

    create user db & try CRUD pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+
+  resources :users
+  root 'users#index'
+  #root 'application#hello'
   #root 'application#goodbye'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
